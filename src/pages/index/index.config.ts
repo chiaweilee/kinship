db06275b6330bca77c34d2bee0a02d8babc99a65
lijiawei <ljw@live.jp>
2024-01-17 10:24:49 +0800

    feat: canvas

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,3 +1,6 @@
 export default definePageConfig({
-  navigationBarTitleText: '首页'
-})
+  navigationBarTitleText: '首页',
+  usingComponents: {
+    'f6-canvas': '../../../miniprogram_npm/@antv/f6-wx/canvas/canvas',
+  },
+});
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,35 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.less'
+import { useState } from 'react';
+import { View, Text } from '@tarojs/components';
+import { useLoad } from '@tarojs/taro';
+import F6 from '@antv/f6-wx/src/index';
+import './index.less';
 
 export default function Index() {
+  const [canvasWidth, $canvasWidth] = useState();
+  const [canvasHeight, $canvasHeight] = useState();
+  const [canvasPixelRatio, $canvasPixelRatio] = useState();
 
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    const { windowWidth, windowHeight, pixelRatio } = wx.getSystemInfoSync();
+    $canvasWidth(windowWidth);
+    $canvasHeight(windowHeight);
+    $canvasPixelRatio(pixelRatio);
+  });
+
+  const handleInit = (event) => {
+    const { ctx, canvas, renderer } = event.detail;
+    console.log(ctx);
+  };
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
+      <f6-canvas
+        width={canvasWidth}
+        height={canvasHeight}
+        pixelRatio={canvasPixelRatio}
+        onInit={handleInit}
+      />
     </View>
-  )
+  );
 }
